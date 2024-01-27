69f186f6caaf462b66e7965791d0e43293e7f78a
Michael S.O.S Johnson <113233530+SOSJ001@users.noreply.github.com>
2024-01-27 08:55:10 +0000

    Updated metatags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,16 @@ const WenChart = dynamic(
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>$Wen/USDC X S.O.S</title>
+        <meta name="description" content="Helps you know what is going on with $wen/USDC" />
+        <meta property="og:title" content="$Wen/USDC X S.O.S" />
+        <meta property="og:description" content="$Wen/USDC X S.O.S" />
+        <meta property="og:image" content={`https://wenchart-sos-being-nice.vercel.app/OG.png`} />
+        <meta property="og:url" content='https://wenchart-sos-being-nice.vercel.app/' />
+        <meta property="og:type" content="website" />
+        {/* Other meta tags as needed */}
+      </Head>
       <WenChart />
     </div>
   );
