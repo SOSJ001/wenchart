03defd5c898f43331ab5a9da3736c4dafa4c74b5
Michael S.O.S Johnson <113233530+SOSJ001@users.noreply.github.com>
2024-01-27 07:58:24 +0000

    version-1

---
+++ src/app/components/wenchart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function wenchart() {
+  return (
+    <div>
+      <div id="dexscreener-embed">
+        <iframe src="https://dexscreener.com/solana/6vHCZP6Nytp2pn4P9VkK8hFZCecaojvx4vUqNRWQ8YLw?embed=1&theme=dark&info=0"></iframe>
+      </div>
+    </div>
+  )
+}
+
+export default wenchart
